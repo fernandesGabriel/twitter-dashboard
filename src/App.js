10c84497b83c629b5ca900d2be764365cc2663f4
Gabriel Fernandes <gabriel_fernandes1993@hotmail.com>
2020-05-21 14:24:21 +0200

    Init socketio client

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import socketIOClient from 'socket.io-client';
+
+const ENDPOINT = 'localhost:3002';
 
 function App() {
+  const [response, setResponse] = useState('');
+
+  useEffect(() => {
+    const socket = socketIOClient(ENDPOINT);
+
+    socket.on('FromAPI', data => {
+      setResponse(data);
+    });
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        <p> Twitter Dasboard </p>
+        <p> Twitter Dasboard {response}</p>
       </header>
     </div>
   );
