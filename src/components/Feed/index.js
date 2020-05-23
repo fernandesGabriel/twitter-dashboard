bd432985af1bb95324b7ee8be7144efecb6e5ce8
Gabriel Fernandes <gabriel_fernandes1993@hotmail.com>
2020-05-23 18:59:40 +0200

    Add limit to feed list

--- src/components/Feed/index.js
+++ src/components/Feed/index.js
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from 'react';
 import socketClient from 'socket.io-client';
 import Card from '../Card';
 
+const FEED_MAX_SIZE = 100;
+
 export default class Feed extends React.Component {
   constructor(props) {
     super(props);
@@ -20,14 +22,19 @@ export default class Feed extends React.Component {
 
   feedDidChange(data) {
     const feed = this.state.feed;
+
     feed.unshift({ text: data });
+    if (feed.length > FEED_MAX_SIZE) {
+      feed.splice(-1,1);
+    }
+
     this.setState({ feed: feed });
   }
 
   render() {
     return (
       <div>
-        {this.state.feed.map((post) => { return <Card text={post.text} /> })}
+        {this.state.feed.map((post) => <Card {...post} /> )}
       </div>
     );
   }
